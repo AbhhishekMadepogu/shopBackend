8ec4bfb991699600008935c5016d1f2fec03feca
Abhishek09-M <abhishek@codepxldev.com>
2022-10-03 22:54:30 -0400

    added env and connected to mongo db

--- app.js
+++ app.js
@@ -1,6 +1,32 @@
 const express=require('express');
+const morgan = require('morgan');
+const mongoose=require('mongoose');
+
 const app=express()
-app.get('/',(req,res)=>{res.send('Hello API !');})
+require('dotenv/config');
+const api=process.env.API_URL
+
+//middleware
+app.use(express.json())
+app.use(morgan('tiny'))
+
+app.get(`${api}/products`,(req,res)=>{
+    const product={
+        id:1,
+        name:'hair dresser',
+        image:'someurl',
+    }
+    res.send(product);
+})
+console.log(process.env.CONNECTION_STRING.slice(1,-1))
+mongoose.connect(process.env.CONNECTION_STRING).then(()=>{console.log('Database connection is ready')})
+.catch((err)=>{console.log(err)
+    console.log(process.env.CONNECTION_STRING)})
+app.post(`${api}/products`,(req,res)=>{
+    const newProduct=req.body
+    console.log(newProduct)
+    res.send(newProduct);
+})
 app.listen(3000,()=>{
     console.log('Server running http://localhost:3000');
 })
