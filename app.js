e69fb2aa86c2310a0136f5bea15cdf0e527d48a1
Abhishek09-M <abhishek@codepxldev.com>
2022-10-04 00:39:03 -0400

    adding product to backend mongo db

--- app.js
+++ app.js
@@ -10,23 +10,40 @@ const api=process.env.API_URL
 app.use(express.json())
 app.use(morgan('tiny'))
 
+const productSchema=mongoose.Schema({
+    name:String,
+    image:String,
+    countInStock:Number
+})
+const Product=mongoose.model('Product',productSchema);
+
 app.get(`${api}/products`,(req,res)=>{
     const product={
         id:1,
         name:'hair dresser',
         image:'someurl',
     }
+
     res.send(product);
 })
-console.log(process.env.CONNECTION_STRING.slice(1,-1))
-mongoose.connect(process.env.CONNECTION_STRING).then(()=>{console.log('Database connection is ready')})
+app.post(`${api}/products`,(req,res)=>{
+    const newProduct=new Product({
+        name:req.body.name,
+        image:req.body.image,
+        countInStock:req.body.countInStock
+    })
+    newProduct.save().then((createdProduct=>{
+        res.status(201).json(createdProduct)
+    })).catch((err)=>res.status(500).json({
+        error:err,
+        success:false
+    }))
+})
+mongoose.connect(process.env.CONNECTION_STRING,{})
+.then(()=>{console.log('Database connection is ready')})
 .catch((err)=>{console.log(err)
     console.log(process.env.CONNECTION_STRING)})
-app.post(`${api}/products`,(req,res)=>{
-    const newProduct=req.body
-    console.log(newProduct)
-    res.send(newProduct);
-})
+
 app.listen(3000,()=>{
     console.log('Server running http://localhost:3000');
 })
